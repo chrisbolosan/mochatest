76dc7006af7b75b0b0d70741fc524b89f3cef17c
chrisbolosan <cbolosan@gmail.com>
2021-10-12 17:45:40 -0500

    index with tasks

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const tasks = [
+  {
+    id: 1,
+    name: 'Task 1',
+    completed: false,
+  },
+  {
+    id: 1,
+    name: 'Task 2',
+    completed: false,
+  },
+  {
+    id: 1,
+    name: 'Task 3',
+    completed: false,
+  },
+];
+//GET
+
+//POST
+
+//PUT
+
+//PATCH
+
+//DELETE
