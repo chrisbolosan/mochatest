0b9460a8895e50eb8a69b5a57130cc07188c6b03
chrisbolosan <cbolosan@gmail.com>
2021-10-13 16:47:45 -0500

    dependencies with imports

---
+++ test/task.js
@@ -0,0 +1,17 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+
+//Assertion style
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Tasks API', () => {
+  //Test GET Route
+  //Test GET Route by ID
+  //Test POST Route
+  //Test PUT Route
+  //Test PATCH Route
+  //Test DELETE Route
+});
